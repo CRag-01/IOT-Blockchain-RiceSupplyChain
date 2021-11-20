72b280cd83086406f741335fb1f2881095f9f842
ayan2809 <ayansadhukhan28@gmail.com>
2021-11-20 15:32:01 +0530

    Error Modal

--- blockchain_flask/blockchain_client/static/js/script2.js
+++ blockchain_flask/blockchain_client/static/js/script2.js
@@ -7,13 +7,21 @@ document.querySelector('.submit-xD').addEventListener('click', function () {
   document.querySelector('.cont').classList.toggle('s--signup');
 });
 
+// SUCCESS MODAL PART 
 var $loader = $(".sign-up");
 var $btnDone = $loader.find(".done");
 var $loadingMessage = $loader.find(".loader-message");
-
+var $btnRetry = $loader.find(".retry");
+var $btnCancel = $loader.find(".cancel");
 
 $(document).ready(function () {
   $btnDone.click(function () {
     $('.success').fadeOut(500);
+  });
+});
+
+$(document).ready(function () {
+  $btnCancel.click(function () {
+    $('.error').fadeOut(500);
   });
 });
